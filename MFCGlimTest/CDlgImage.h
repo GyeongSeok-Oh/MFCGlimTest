f9beca68415b639ec7c7a5702ac41130cf91cc49
GyeongSeok-Oh <dhrudtjr945@gmail.com>
2023-11-27 19:20:13 +0900

    초기 커밋.

---
+++ MFCGlimTest/CDlgImage.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "afxdialogex.h"
+#include <iostream>
+using namespace std;
+
+// CDlgImage 대화 상자
+
+class CDlgImage : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDlgImage)
+
+public:
+	CDlgImage(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CDlgImage();
+
+	CWnd* m_pParent;
+	CImage m_Image;
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CDlgImage };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	void initImage();
+	afx_msg void OnBnClickedCancel();
+	void DrawCircle(CDC* cd, int x, int y, int radius);
+	void DrawCross(CDC* cd, int x, int y, int radius);
+
+};
---
+++ MFCGlimTest/MFCGlimTestDlg.h
@@ -0,0 +1,46 @@
+﻿
+// MFCGlimTestDlg.h: 헤더 파일
+
+#include "CDlgImage.h"
+#include <iostream>
+using namespace std;
+#pragma once
+
+
+// CMFCGlimTestDlg 대화 상자
+class CMFCGlimTestDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCGlimTestDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	CFont m_TotalFont;
+	CDlgImage* m_pDlgImage;
+	
+private:
+	CImage m_image;
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCGLIMTEST_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+
+public:
+	afx_msg void OnBnClickedPlay();
+	afx_msg void OnDestroy();
+};
